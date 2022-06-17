280a037df8bcc3df52b42f432832b5bded3b9d7e
SamueleLettieri <samuele2073@gmail.com>
2022-06-17 17:13:09 +0200

    risolto errore di scrittura nel programma per verificare l'email

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
 /* PROGRAMMA PER VERIFICARE L'EMAIL */
 
-/* let email = ["pippo1@gmail.com", "pippo2@gmail.com", "pippo3@gmail.com", "pippo4@gmail.com", "pippo5@gmail.com", "pippo6@gmail.com", "pippo7@gmail.com", "pippo8@gmail.com", "pippo9@gmail.com", "pippo10@gmail.com", "pippo11@gmail.com", "pippo12@gmail.com", "pippo13@gmail.com", "pippo14@gmail.com", "pippo15@gmail.com"];
+let email = ["pippo1@gmail.com", "pippo2@gmail.com", "pippo3@gmail.com", "pippo4@gmail.com", "pippo5@gmail.com", "pippo6@gmail.com", "pippo7@gmail.com", "pippo8@gmail.com", "pippo9@gmail.com", "pippo10@gmail.com", "pippo11@gmail.com", "pippo12@gmail.com", "pippo13@gmail.com", "pippo14@gmail.com", "pippo15@gmail.com"];
 console.log(email)
 
 const userEmail = prompt("iserisci la tua email");
 console.log(userEmail);
 
-let emailFaund = "dopo un atento controllo, l'email inserita non è stata accetata";
+let emailFaund = "dopo un attento controllo, l'email inserita non è stata accetata";
 
 for (let i = 0; i < email.length; i++){
     let allEmail = email[i];
     console.log(allEmail)
 
     if ( userEmail === allEmail ){
-        emailFaund = "email ferificata, puoi accedere";
+        emailFaund = "email verificata, puoi accedere";
     }
 }
 
-alert(emailFaund); */
+alert(emailFaund); 
 
-
+ 
 
 
 
