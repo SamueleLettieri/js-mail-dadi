4e5fa23ead787e85881882b631da8cab28476e70
SamueleLettieri <samuele2073@gmail.com>
2022-06-17 17:10:27 +0200

    programma per giocare a dadi funzionante in console

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-let email = ["pippo1@gmail.com", "pippo2@gmail.com", "pippo3@gmail.com", "pippo4@gmail.com", "pippo5@gmail.com", "pippo6@gmail.com", "pippo7@gmail.com", "pippo8@gmail.com", "pippo9@gmail.com", "pippo10@gmail.com", "pippo11@gmail.com", "pippo12@gmail.com", "pippo13@gmail.com", "pippo14@gmail.com", "pippo15@gmail.com"];
+/* PROGRAMMA PER VERIFICARE L'EMAIL */
+
+/* let email = ["pippo1@gmail.com", "pippo2@gmail.com", "pippo3@gmail.com", "pippo4@gmail.com", "pippo5@gmail.com", "pippo6@gmail.com", "pippo7@gmail.com", "pippo8@gmail.com", "pippo9@gmail.com", "pippo10@gmail.com", "pippo11@gmail.com", "pippo12@gmail.com", "pippo13@gmail.com", "pippo14@gmail.com", "pippo15@gmail.com"];
 console.log(email)
 
 const userEmail = prompt("iserisci la tua email");
@@ -15,4 +17,29 @@ for (let i = 0; i < email.length; i++){
     }
 }
 
-alert(emailFaund);
+alert(emailFaund); */
+
+
+
+
+
+/* PROGRAMMA PER GIOCARE A DADI */
+
+const userDice = parseInt(Math.random() * 5)+1;
+console.log(userDice)
+
+let message
+
+const  aiDice = parseInt(Math.random() * 5)+1;
+console.log(aiDice)
+
+if(userDice > aiDice){
+    message = "Congratulazioni hai vinto! :)";
+    console.log(message);
+} else if(userDice < aiDice){
+    message = "Spiacente ha vinto il computer, ri tenta ;)";
+    console.log(message);
+} else if(userDice == aiDice){
+    message = "Che peccato, e finita in pareggio :(";
+    console.log(message);
+}
